5335d126f925f443d3278e5f8c15e49708559ef2
Matt Newton <newton10471@gmail.com>
2012-08-15 09:08:58 -0400

    working data model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :title
 
-  has_many :tags
+  has_and_belongs_to_many :tags
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120815014427) do
+ActiveRecord::Schema.define(:version => 20120816014413) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.string   "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "posts_tags", :id => false, :force => true do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
   end
 
   create_table "tags", :force => true do |t|
